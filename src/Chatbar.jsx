1bf92d1d61a2567163d7889d358f781a85cfb3b4
Ben Deltenre <benjdelt@gmail.com>
2018-09-04 18:51:46 +0000

    Added initial state and props

--- src/Chatbar.jsx
+++ src/Chatbar.jsx
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
 class ChatBar extends Component {
+
   render() {
     return (
         <footer className="chatbar">
-          <input className="chatbar-username" placeholder="Your Name (Optional)" />
+          <input className="chatbar-username" placeholder={this.props.currentUser} />
           <input className="chatbar-message" placeholder="Type a message and hit ENTER" />
         </footer>
     );
