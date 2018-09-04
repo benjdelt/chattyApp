97765cecb3ebabb8203c7ef6106868a73fc40d03
Ben Deltenre <benjdelt@gmail.com>
2018-09-04 02:37:14 +0000

    Initial setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import React, {Component} from 'react';
+import NavBar from './Navbar.jsx';
+import ChatBar from './Chatbar.jsx';
+import MessageList from './MessageList.jsx';
+
 
 class App extends Component {
   render() {
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <NavBar />
+        <MessageList />
+        <ChatBar /> 
+      </div>
     );
   }
-}
+} 
 export default App;
